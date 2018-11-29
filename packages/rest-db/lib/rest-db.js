449f97df73c9fd188793b02af6dacde0089a993f
Laurence <laurence@hubdoc.com>
2018-11-29 14:00:05 -0500

    rest api stuff

--- packages/rest-db/lib/rest-db.js
+++ packages/rest-db/lib/rest-db.js
@@ -2,6 +2,26 @@
 
 module.exports = restDb;
 
+const express = require('express')
+const app = express()
+const port = 3000
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/email_signup', upload.array(), (req, res) => {
+    const email = req.body.email;
+    const fName = req.body.fname;
+    const lName = req.body.lname;
+    res.send(`Email: ${email} \nName: ${fName} ${lName}`)
+    }
+)
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
 function restDb() {
     // TODO
 }
