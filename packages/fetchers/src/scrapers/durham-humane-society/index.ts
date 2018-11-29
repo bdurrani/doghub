ec5d8b7abd55e7f396fb415f9681ea7a66cdf7f2
BIlal Durrani <bilal@hubdoc.com>
2018-11-29 11:54:16 -0500

    make project runnable

--- packages/fetchers/src/scrapers/durham-humane-society/index.ts
+++ packages/fetchers/src/scrapers/durham-humane-society/index.ts
@@ -15,10 +15,7 @@ export class DurhamSocietyFetcher extends FetcherBase {
 
   async start() {
     this.browser = await puppeteer.launch({
-      // Launch chromium using a proxy server on port 9876.
-      // More on proxying:
-      //    https://www.chromium.org/developers/design-documents/network-settings
-      // args: ["--proxy-server=127.0.0.1:9876"]
+      headless: false
     });
     const page = await this.browser.newPage();
     await page.goto(this.url);
