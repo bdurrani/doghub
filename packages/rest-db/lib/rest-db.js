a5a605c3f492f4f5b91e57bd3c82e56bf644a448
BIlal Durrani <bilal@hubdoc.com>
2018-11-29 21:33:56 -0500

    run rest api using pm2 for auto restart

--- packages/rest-db/lib/rest-db.js
+++ packages/rest-db/lib/rest-db.js
@@ -1,29 +1,24 @@
-'use strict';
+"use strict";
 
-module.exports = restDb;
-
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = 3000
-var bodyParser = require('body-parser');
-var multer = require('multer');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+var bodyParser = require("body-parser");
+var multer = require("multer");
 var upload = multer();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.post('/email_signup', (req, res) => {
-    console.log("POST")
-    console.log(JSON.stringify(req.body.email));
-    const email = req.body.email;
-    res.json({email});
-    }
-)
+app.post("/email_signup", (req, res) => {
+  console.log("POST");
+  console.log(JSON.stringify(req.body.email));
+  const email = req.body.email;
+  res.json({ email });
+});
 
-app.listen(port, '0.0.0.0', () => console.log(`App listening on port ${port}!`))
-
-function restDb() {
-    // TODO
-}
+app.listen(port, "0.0.0.0", () =>
+  console.log(`App listening on port ${port}!`)
+);
